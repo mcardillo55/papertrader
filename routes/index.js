9d299631d62626a8a512fda7aaf0428a63442150
Mike Cardillo <mcardillo55@gmail.com>
2017-01-10 12:03:30 -0800

    Added recent portfolios to index view, with links to their view pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { user : req.user });
+  Account.find({}).select('portfolios').exec(function(err, query) {
+    var allPortfolios = Array();
+    for (var idx=0; idx<query.length; idx++){
+      allPortfolios = allPortfolios.concat(query[idx].portfolios);
+    }
+    res.render('index', { user : req.user, portfolios : allPortfolios });
+  });
 });
 
 module.exports = router;
