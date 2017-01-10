4984f80cc8ce13e1d6e2a92d884af19d7aa8ce1d
Mike Cardillo <mcardillo55@gmail.com>
2017-01-10 12:02:59 -0800

    Added 'view portfolio' view

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -2,7 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 var router = express.Router();
-var Portfolio = require('../models/portfolio').Portfolio;
+var Account = require('../models/account');
 var Hashids = require('hashids');
 
 /* GET portfolios page. */
@@ -29,6 +29,14 @@ router.post('/new', ensureLoggedIn('/login'), function(req, res, next) {
   req.user.portfolios.push(newPortfolio);
   req.user.save();
   res.redirect('/portfolios');
+});
+
+router.get('/view', function(req, res, next) {
+  var hashid = req.query.id;
+  Account.findOne({}).where('portfolios.hashid').equals(hashid).select('portfolios.$').exec( function(err, portfolio) {
+    var stocks = portfolio.portfolios[0].stocks;
+    res.render('viewPortfolio', { user : req.user, stocks : stocks });
+  });
 });
 
 module.exports = router;
