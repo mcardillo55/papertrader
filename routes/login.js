3c4e70bc52a8df6109a5d1e94dc0b367659372a8
Mike Cardillo <mcardillo55@gmail.com>
2017-01-09 23:59:20 -0800

    Initial Commit

---
+++ routes/login.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+/* GET login page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+module.exports = router;
